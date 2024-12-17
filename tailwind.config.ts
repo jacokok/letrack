ffc87976ba6530cba008d2241627e60dfa18ef56
Jaco Kok <kokjaco2@gmail.com>
2024-12-17 22:22:00 +0200

    Adding chart capability

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -5,9 +5,21 @@ import tailwindcssAnimate from "tailwindcss-animate";
 
 export default {
 	darkMode: ["class"],
-	content: ["./src/**/*.{html,js,svelte,ts}", "./node_modules/@kayord/ui/**/*.{html,js,svelte,ts}"],
+	content: [
+		"./src/**/*.{html,js,svelte,ts}",
+		"./node_modules/@kayord/ui/**/*.{html,js,svelte,ts}",
+		"./node_modules/layerchart/**/*.{svelte,js}"
+	],
 	theme: {
 		extend: {
+			colors: {
+				surface: {
+					content: "hsl(var(--card-foreground))",
+					100: "hsl(var(--background))",
+					200: "hsl(var(---muted))",
+					300: "hsl(var(--background))"
+				}
+			},
 			fontFamily: {
 				sans: [...fontFamily.sans]
 			}
