d4cafc7e9ff83271c9aacabb5656508c03505943
Jaco Kok <kokjaco2@gmail.com>
2025-04-25 11:24:15 +0200

    Latest conf

--- sensor/example-config.py
+++ sensor/example-config.py
@@ -1,6 +1,13 @@
-WIFI_SSID = "wifissd"
-WIFI_PASSWD = "wifipassword"
-MQTT_SERVER = "10.0.0.2"
-NTP_HOST = "time.google.com"  # or ip of ntp server if no internet on network. Example in docker-compose.yml
+# WIFI_SSID = "wifissd"
+# WIFI_PASSWD = "wifipassword"
+# MQTT_SERVER = "10.0.0.2"
+# NTP_HOST = "time.google.com"  # or ip of ntp server if no internet on network. Example in docker-compose.yml
+# BEAM1 = 1
+# BEAM2 = 2
+
+WIFI_SSID = "letrack"
+WIFI_PASSWD = "letrack123"
+MQTT_SERVER = "192.168.0.100"
+NTP_HOST = "192.168.0.100"
 BEAM1 = 1
 BEAM2 = 2
